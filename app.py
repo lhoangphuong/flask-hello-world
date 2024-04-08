4ed6c7c1835d4a6ed40b803eace1671ca0ed2620
lhoangphuong <49200808+lhoangphuong@users.noreply.github.com>
2024-04-08 23:23:21 +0700

    add index.html

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+def index():
+  pagetitle = "HomePage"
+  return render_template("index.html",
+    mytitle="HomePage", 
+    mycontent="Hello World"
+  )
