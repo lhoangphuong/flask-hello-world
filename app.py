d459af8fd5b3eaf0045f86369ec990575e2d757b
lhoangphuong <49200808+lhoangphuong@users.noreply.github.com>
2024-04-09 23:07:37 +0700

    merge POST  and GET

--- app.py
+++ app.py
@@ -10,25 +10,23 @@ def hello_world():
   return "<p>Hello, World!</p>"
 
 
-@app.route('/cognito_auth', methods=['GET'])
-def form():
-    return render_template('cognito_auth.html')
-
-
-@app.route('/cognito_auth', methods=['POST'])
+@app.route('/cognito_auth', methods=['GET', 'POST'])
 def cognito_auth():
-    url = 'https://cognito-idp.us-east-1.amazonaws.com/'
-    headers = {
-        'Content-Type': 'application/x-amz-json-1.1',
-        'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth'
-    }
-    data = {
-        "AuthFlow": "USER_PASSWORD_AUTH",
-        "AuthParameters": {
-            "USERNAME": request.form.get('username'),
-            "PASSWORD": request.form.get('password')
-        },
-        "ClientId": request.form.get('client_id')
-    }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    return jsonify(response.json())
+    if request.method == 'GET':
+        return render_template('cognito_auth.html')
+    elif request.method == 'POST':
+        url = 'https://cognito-idp.us-east-1.amazonaws.com/'
+        headers = {
+            'Content-Type': 'application/x-amz-json-1.1',
+            'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth'
+        }
+        data = {
+            "AuthFlow": "USER_PASSWORD_AUTH",
+            "AuthParameters": {
+                "USERNAME": request.form.get('username'),
+                "PASSWORD": request.form.get('password')
+            },
+            "ClientId": request.form.get('client_id')
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        return jsonify(response.json())
