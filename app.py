5f905637fc094001ff53960e5ac9dac88b0789d1
lhoangphuong <49200808+lhoangphuong@users.noreply.github.com>
2024-04-09 18:40:12 +0700

    update new version

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from backend.database_query import *
+import requests
+import json
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return render_template('home.html')
+@app.route("/")
+def hello_world():
+  return "<p>Hello, World!</p>"
 
-@app.route('/login')
-def login():
-  return render_template('login.html')
 
-@app.route('/signUp')
-def signUp():
-  return render_template('signUp.html')
+@app.route('/cognito_auth', methods=['GET'])
+def form():
+    return render_template('cognito_auth.html')
 
-@app.route('/home')
-def home():
-  return render_template('home.html')
+
+@app.route('/cognito_auth', methods=['POST'])
+def cognito_auth():
+    url = 'https://cognito-idp.us-east-1.amazonaws.com/'
+    headers = {
+        'Content-Type': 'application/x-amz-json-1.1',
+        'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth'
+    }
+    data = {
+        "AuthFlow": "USER_PASSWORD_AUTH",
+        "AuthParameters": {
+            "USERNAME": request.form.get('username'),
+            "PASSWORD": request.form.get('password')
+        },
+        "ClientId": request.form.get('client_id')
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    return jsonify(response.json())
